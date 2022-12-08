db2faf8cc46288c370e51444c6c13bdc395b3690
Ehtisham Mushtaq <ehtishammushtaq@gmail.com>
2022-12-08 03:57:22 -0600

    added Password_validator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Passwordbox from './components/passwordbox';
+import './App.css';
+
+function App() {
+  return (
+    
+   <div>
+     <Passwordbox/>
+   </div>
+    
+      
+  );
+}
+
+export default App;
